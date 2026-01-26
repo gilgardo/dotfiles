76991e166a7a4d117c2fa1869d4baf9f7fbd59f3
alessandro foresta <fore.ale.1997@gmail.com>
2026-01-26 10:44:55 +0100

    feat: add nvim config with Next.js/TypeScript/Tailwind support

---
+++ .config/nvim/lua/plugins/nextjs.lua
@@ -0,0 +1,77 @@
+-- Next.js / React / TypeScript / Tailwind development support
+return {
+  -- TypeScript support via LazyVim extra (includes vtsls LSP)
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- Tailwind CSS support (autocomplete, color preview, class sorting)
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+
+  -- JSON support (for package.json, tsconfig.json, etc.)
+  { import = "lazyvim.plugins.extras.lang.json" },
+
+  -- Additional treesitter parsers for Next.js
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "javascript",
+        "html",
+        "css",
+        "json",
+        "jsonc",
+        "markdown",
+        "markdown_inline",
+      })
+    end,
+  },
+
+  -- Mason: ensure LSP servers and formatters are installed
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- TypeScript/JavaScript
+        "typescript-language-server",
+        "eslint-lsp",
+        "prettierd",
+        -- Tailwind
+        "tailwindcss-language-server",
+        -- CSS
+        "css-lsp",
+      },
+    },
+  },
+
+  -- Configure formatters
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        json = { "prettierd" },
+        jsonc = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        markdown = { "prettierd" },
+      },
+    },
+  },
+
+  -- Configure linters
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        javascript = { "eslint" },
+        javascriptreact = { "eslint" },
+        typescript = { "eslint" },
+        typescriptreact = { "eslint" },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/gilgardo/.config/omarchy/current/theme/neovim.lua
